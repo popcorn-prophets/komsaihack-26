28088c6534b83fcb1dc6a523ee183f4a33d19e0e
TheAbyssStaresBack <68632990+TheAbyssStaresBack@users.noreply.github.com>
2026-04-06 20:00:04 +0800

    fix(ui): fix dropdown improperly displaying

--- components/control-center/incidents/report-view/report-list/incident-sort.tsx
+++ components/control-center/incidents/report-view/report-list/incident-sort.tsx
@@ -21,6 +21,17 @@ interface IncidentSorterProps {
 export function IncidentSorter({ onChangeSort }: IncidentSorterProps) {
   const [sortBy, setSortBy] = React.useState('incident_time');
   const [sortOrder, setSortOrder] = React.useState('descending');
+  const [triggerWidth, setTriggerWidth] = React.useState<number>(0);
+  const triggerRef = React.useRef<HTMLButtonElement>(null);
+
+  React.useEffect(() => {
+    const updateWidth = () => {
+      if (triggerRef.current) {
+        setTriggerWidth(triggerRef.current.offsetWidth);
+      }
+    };
+    updateWidth();
+  });
 
   function changeSortBy(sortBy: string) {
     setSortBy(sortBy);
@@ -35,9 +46,11 @@ export function IncidentSorter({ onChangeSort }: IncidentSorterProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">Sort</Button>
+        <Button ref={triggerRef} variant="outline">
+          Sort
+        </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-full">
+      <DropdownMenuContent align="start" style={{ width: `${triggerWidth}px` }}>
         <DropdownMenuGroup>
           <DropdownMenuLabel>Sort By</DropdownMenuLabel>
           <DropdownMenuRadioGroup value={sortBy} onValueChange={changeSortBy}>
